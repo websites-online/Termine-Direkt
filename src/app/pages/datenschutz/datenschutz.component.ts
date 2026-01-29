147bd9e3a6ee140d3ac2b29c3829466298f06786
danOdo <webdesignodobasic@gmail.com>
2026-01-29 13:38:12 +0100

    impressum

--- src/app/pages/datenschutz/datenschutz.component.ts
+++ src/app/pages/datenschutz/datenschutz.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-datenschutz',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './datenschutz.component.html',
   styleUrl: './datenschutz.component.css'
 })
--- src/app/pages/impressum/impressum.component.ts
+++ src/app/pages/impressum/impressum.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.css'
 })
