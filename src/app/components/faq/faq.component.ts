85bb7ebe45a65d875f21c1ec826f0129808080f1
danOdo <webdesignodobasic@gmail.com>
2026-09-15 13:26:19 +0200

    neue faq

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrl: './faq.component.css',
 })
 export class FaqComponent {}
